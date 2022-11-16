e389cf77f9d3678840a6e1f4721e00028d010352
AgaffDL <118319500+AgaffDL@users.noreply.github.com>
2022-11-16 19:14:12 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-DEVICE_PATH := device/xiaomi/lisa
+DEVICE_PATH := device/realme/RMX3461
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
